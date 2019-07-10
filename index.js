b9cf4ad310d2c7e1fdd086d1e63ec378c97946de
juliachaush <51079330+juliachaush@users.noreply.github.com>
2019-07-10 13:16:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let calcul = prompt('Sum Numbers', 0);
+
+function sumNumbers (calcul) {
+    if (calcul>0){
+        alert('Yes!');
+    } else {
+        alert("No");
+    }
+}
+
+sumNumbers(calcul)
